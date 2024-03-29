c6b667f97607ba6c1ffdc360f666962e49dcfd2a
ken <a1821010@yahoo.com.tw>
2024-03-29 10:47:46 +0800

    initial commit

---
+++ app/src/main/java/com/ken/bmi/Hello.kt
@@ -0,0 +1,16 @@
+package com.ken.bmi
+
+class Hello {
+
+}
+
+fun main() {
+    println("Hello Kotlin@@")
+    val age =20//cant Change
+    println(age.dec())
+    println(age.inc())
+    var name = "Ken" //var can change
+    println(name.get(1))
+    name = "Tsai"
+    println(name)
+}
