29cec63ed52d44491f7453ebceac177e783c01f5
ken <a1821010@yahoo.com.tw>
2024-03-30 17:19:26 +0800

    GuessHW

--- app/src/main/java/com/ken/bmi/GuassHW.kt
+++ app/src/main/java/com/ken/bmi/GuassHW.kt
@@ -1,35 +1,33 @@
 package com.ken.bmi
 
-import kotlin.random.Random
-
 fun main() {
-
-//    println(Random.nextInt(1,11)) //11 not above
+    //    println(Random.nextInt(1,11)) //11 not above
 //    println((1..10).random())
     val secrete = (1..10).random()
     println("serect: $secrete")
     var num =0
-//    while(num != secrete) {
     var bingo = false
-    for(i in 1..3) {
-        println("PLZ enter a number bwn 1 to 10")
+    while(num != secrete) {
+
+//    for(i in 1..3) {
+        if (!bingo) {
+            println("PLZ enter a number between 1 to 10")
+            bingo = true
+        }
         val input = readLine()
         num = input?.toIntOrNull() ?: -1//?防null
         //num same name OK
         println("Ur Num is: $num ")//${input?.length}
 
         if (num < secrete) {
-            println("Bigger")//${input?.length}
+            println("PLZ enter a number between $num to 10")//${input?.length}
         } else if (num > secrete) {
-            println("Smaller")
+            println("PLZ enter a number between 1 to $num")
         } else {
-            println("LUCK")
-            bingo = true
+            println("U got it")
+
             break  //leave nearest loop
         }
-    }
-    if (!bingo)
-        println("Failed, its $secrete")
-//    }
 
+    }
 }
--- app/src/main/java/com/ken/bmi/Variable.kt
+++ app/src/main/java/com/ken/bmi/Variable.kt
@@ -1,13 +1,28 @@
 package com.ken.bmi
 
 fun main() {
-    var name:String? ="Ken"
+    val array = intArrayOf(2,5,8)
+    array.size
+    println(array[1])
+    val ss = arrayOf("SUN", "MON", "TUE")
+    println(ss.size)
+    println(ss.get(0))
+    for(day in ss){
+        println(day)
+    }
+    //For
+    val range = 0 ..10
+    for(i in 1 ..30 step 2)
+    println(i)
 
-    if (name != null) {
-        println(name.length)
-    } //? if not null => length
-    println(name?.get(0))//? if not null => get
-    println( name ?: 0 ) //貓王表示法
+
+//    var name:String? ="Ken"
+//
+//    if (name != null) {
+//        println(name.length)
+//    } //? if not null => length
+//    println(name?.get(0))//? if not null => get
+//    println( name ?: 0 ) //貓王表示法
 //    name = null
 
 //    var counter = 0;
